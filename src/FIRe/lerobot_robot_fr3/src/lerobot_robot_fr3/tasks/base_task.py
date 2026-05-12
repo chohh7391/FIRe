83bb11a4b1c4a2da4458685d731df33a86dce77f
chohh7391 <chohh7391@gmail.com>
2026-05-12 12:51:05 +0900

    modified:   README.md 	deleted:    scripts/compare_obs.py 	deleted:    scripts/dummy_inference.py 	modified:   scripts/play.py 	deleted:    scripts/play_factory.py 	deleted:    scripts/play_forge.py 	deleted:    scripts/play_with_csv.py 	deleted:    scripts/plot_campare_factory.py 	deleted:    scripts/plot_campare_forge.py 	new file:   scripts/plot_data.py 	deleted:    scripts/plot_log.py 	modified:   src/FIRe/lerobot_robot_fr3/src/lerobot_robot_fr3/tasks/base_task.py 	modified:   src/FIRe/lerobot_robot_fr3/src/lerobot_robot_fr3/tasks/factory/factory.py

--- src/FIRe/lerobot_robot_fr3/src/lerobot_robot_fr3/tasks/base_task.py
+++ src/FIRe/lerobot_robot_fr3/src/lerobot_robot_fr3/tasks/base_task.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Dict
 import numpy as np
 from abc import ABC, abstractmethod
 from lerobot_robot_fr3.utils import RobotStateManager
@@ -7,7 +7,14 @@ from lerobot_robot_fr3.utils import RobotStateManager
 class Task(ABC):
     def __init__(self, name: str):
         self.name = name
-        self.robot: RobotStateManager = None
+        self._robot: RobotStateManager = None
+
+    @property
+    def robot(self):
+        return self._robot
+    
+    def allocate_robot(self, robot: RobotStateManager):
+        self._robot = robot
     
     @abstractmethod
     def create_config(self):
@@ -17,35 +24,41 @@ class Task(ABC):
     def create_buffer(self):
         raise NotImplementedError
     
-    def allocate_robot(self, robot: RobotStateManager):
-        self.robot = robot
-    
     @abstractmethod
     def reset(self):
         raise NotImplementedError
 
     @abstractmethod
-    def get_observation(self) -> dict[str, np.ndarray]:
+    def get_observation(self) -> Dict[str, np.ndarray]:
         raise NotImplementedError
     
     @abstractmethod
-    def get_arm_action(self, action: dict[str, np.ndarray]) -> np.ndarray:
+    def get_arm_action(self, action: Dict[str, np.ndarray]) -> np.ndarray:
         raise NotImplementedError
 
     @abstractmethod
-    def get_gripper_action(self, action: dict[str, np.ndarray]) -> np.ndarray:
+    def get_gripper_action(self, action: Dict[str, np.ndarray]) -> np.ndarray:
         raise NotImplementedError
     
-    @property
     @abstractmethod
-    def observation_features(self) -> dict[str, tuple]:
-        raise NotImplementedError
-    
-    @property
-    @abstractmethod
-    def action_features(self) -> dict[str, tuple]:
+    def get_log(self) -> Dict[str, np.ndarray]:
         raise NotImplementedError
     
     @abstractmethod
     def process_action(self, arm_action: np.ndarray, gripper_action: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        raise NotImplementedError
+    
+    @property
+    @abstractmethod
+    def observation_features(self) -> Dict[str, Tuple[int, ...]]:
+        raise NotImplementedError
+    
+    @property
+    @abstractmethod
+    def action_features(self) -> Dict[str, Tuple[int, ...]]:
+        raise NotImplementedError
+    
+    @property
+    @abstractmethod
+    def log_features(self) -> Dict[str, Tuple[int, ...]]:
         raise NotImplementedError
